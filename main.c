fd7d5fabe497c385fe9dd827456de1c3e962903b
Dmitry khromov <devandart@ya.ru>
2015-01-02 14:24:11 +0400

    fix

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include "stm32f10x.h"                  // Device header
 
+int status_led_1;
+int status_led_2;
+int status_led_3;
+
 int main()
 {
+	status_led_1 = 0;
+	status_led_2 = 0;
+	status_led_3 = 0;
+	
   RCC->APB2ENR |= RCC_APB2ENR_IOPAEN; //|= (RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN); // Taktirovanie PA pins s alternativnoy functiey
-	//RCC->APB2ENR |= RCC_APB2ENR_IOPBEN; // Taktirovanime PB
 	
   GPIOA->CRL |= GPIO_CRL_MODE0_1;
   GPIOA->CRL &= ~GPIO_CRL_CNF0_0;
@@ -17,7 +24,7 @@ int main()
 	RCC->APB1ENR |= (RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN | RCC_APB1ENR_TIM4EN);
 	
 	TIM2->PSC = 36000-1;
-	TIM2->ARR = 2000;
+	TIM2->ARR = 1000;
 	TIM2->DIER |= TIM_DIER_UIE;
   TIM2->CR1 |= TIM_CR1_CEN;
 	NVIC_EnableIRQ(TIM2_IRQn);
@@ -29,41 +36,58 @@ int main()
 	NVIC_EnableIRQ(TIM3_IRQn);
 	
 	TIM4->PSC = 36000-1;
-	TIM4->ARR = 2000;
+	TIM4->ARR = 3000;
 	TIM4->DIER |= TIM_DIER_UIE;
   TIM4->CR1 |= TIM_CR1_CEN;
 	NVIC_EnableIRQ(TIM4_IRQn);
 	
-	while(1) {}
+	while(1) {
+		if(status_led_1 == 0) {
+			GPIOA->ODR &= !GPIO_ODR_ODR0;
+		} else {
+			GPIOA->ODR |= GPIO_ODR_ODR0;
+		}
+		
+		if(status_led_2 == 0) {
+			GPIOA->ODR &= !GPIO_ODR_ODR1;
+		} else {
+			GPIOA->ODR |= GPIO_ODR_ODR1;
+		}
+		
+		if(status_led_3 == 0) {
+			GPIOA->ODR &= !GPIO_ODR_ODR2;
+		} else {
+			GPIOA->ODR |= GPIO_ODR_ODR2;
+		}
+	}
 }
 
 void TIM2_IRQHandler(void)
 {
 	TIM2->SR &= ~TIM_SR_UIF;
-  if(GPIOA->IDR & GPIO_IDR_IDR0) {
-		GPIOA->ODR &= !GPIO_ODR_ODR0;
+	if(status_led_1 == 0) {
+		status_led_1 = 1;
 	} else {
-		GPIOA->ODR |= GPIO_ODR_ODR0;
+		status_led_1 = 0;
 	}
 }
-
 
 void TIM3_IRQHandler(void)
 {
 	TIM3->SR &= ~TIM_SR_UIF;
-  if(GPIOA->IDR & GPIO_IDR_IDR1) {
-		GPIOA->ODR &= !GPIO_ODR_ODR1;
+	if(status_led_2 == 0) {
+		status_led_2 = 1;
 	} else {
-		GPIOA->ODR |= GPIO_ODR_ODR1;
+		status_led_2 = 0;
 	}
 }
 
 void TIM4_IRQHandler(void)
 {
 	TIM4->SR &= ~TIM_SR_UIF;
-  if(GPIOA->IDR & GPIO_IDR_IDR2) {
-		GPIOA->ODR &= !GPIO_ODR_ODR2;
+  if(status_led_3 == 0) {
+		status_led_3 = 1;
 	} else {
-		GPIOA->ODR |= GPIO_ODR_ODR2;
+		status_led_3 = 0;
 	}
 }
