569d219051ae497d2d3e0bf4898a1043afd5fb27
Dmitry khromov <devandart@ya.ru>
2015-01-03 14:14:09 +0400

    fix

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "stm32f10x.h"                  // Device header
 
 int position;
+int arr;
 
 int main()
 {
+	arr = 500;
 	position = 0;
 	
   RCC->APB2ENR |= RCC_APB2ENR_IOPAEN; //|= (RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN); // Taktirovanie PA pins s alternativnoy functiey
@@ -20,7 +22,7 @@ int main()
 	RCC->APB1ENR |= (RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN | RCC_APB1ENR_TIM4EN);
 	
 	TIM2->PSC = 36000-1;
-	TIM2->ARR = 70;
+	TIM2->ARR = arr;
 	TIM2->DIER |= TIM_DIER_UIE;
   TIM2->CR1 |= TIM_CR1_CEN;
 	NVIC_EnableIRQ(TIM2_IRQn);
@@ -49,7 +51,11 @@ int main()
 
 void TIM2_IRQHandler(void)
 {
+	arr = arr - 20;
+	TIM2->ARR = arr;
+	
 	TIM2->SR &= ~TIM_SR_UIF;
+	
 	if(position == 2) {
 		position = 0;
 	} else {
